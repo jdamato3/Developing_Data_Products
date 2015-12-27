c1bd679a2edadcb446dd65c6ccfbf88a768ce3e9
James D'Amato <james.j.damato@gmail.com>
2015-12-27 16:55:12 -0500

    initial commit of project files

---
+++ ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Data science FTW!"),
+  sidebarPanel(
+    numericInput('id1', 'Numeric input, labeled id1', 0, min = 0, max = 10, step = 1),
+    checkboxGroupInput("id2", "Checkbox",
+                       c("Value 1"= "1",
+                         "Value 2" = "2",
+                         "Value 3" = "3")),
+    dateInput("date", "Date:"),
+    h1('Sidebar panel'),
+    h3('Sidebar text')
+  ),
+  mainPanel(
+    h3('Illustrating outputs'),
+    h4('You entered'),
+    verbatimTextOutput("oid1"),
+    h4('You entered'),
+    verbatimTextOutput("oid2"),
+    h4('You entered'),
+    verbatimTextOutput("odate")
+  )
+))
